e97be6172afa4d624489a23458a1236967f0359a
gabrielslach <gabdl.dev@gmail.com>
2021-10-11 00:57:23 +0800

    :construction: Added initial type definitions

--- server/functions/graphql/schema.js
+++ server/functions/graphql/schema.js
@@ -1,10 +1,13 @@
 const typeDefs = `
     type Query {
-        getCards(sectionID: String, activityID: String): [Card]
+        getCards(sheetInfo: SheetInfo): [Card]
+        getAnswersOfUser(sheetInfo: SheetInfo, userID: String): [Answers]
     }
 
     type Mutation {
         addCards(cards: [Card]): String!
+        addAnswers(sheetInfo: SheetInfo, answers: [Answer]): String!
+        addUser(userInfo: User): String!
     }
 
     type Card {
@@ -12,6 +15,24 @@ const typeDefs = `
         Description: String!
         CorrectAnswer: String
         Choices: [String]
+    }
+
+    type Answer {
+        questionID: String
+        answer: String
+    }
+
+    type SheetInfo {
+        teacherID,
+        sectionID,
+        activityID
+    }
+
+    type User {
+        id: String
+        name: String
+        userType: String
+        section: String
     }
 `;
 
